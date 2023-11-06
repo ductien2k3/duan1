5b8d70235a53c72f8f21ec8a0d049476f5f301c7
ductien2k3 <phamductiendz@gmail.com>
2023-11-06 20:54:00 +0700

    giaodiện + admin

---
+++ admin/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Trang admin</title>
+</head>
+<body>
+  <header>
+    <h1>Trang admin</h1>
+  </header>
+  <div class="main">
+    <div class="sidebar">
+      <ul class="menu">
+        <li><a href="admin.html">Trang Chủ</a></li>
+        <li><a href="danhsach.html">Danh Sách</a></li>
+        <li><a href="sanpham.html">Sản Phẩm</a></li>
+        <li><a href="taikhoan.html">Tài Khoản</a></li>
+        <li><a href="donhang.html">Đơn Hàng</a></li>
+        <li><a href="binhluan.html">Bình Luận</a></li>
+        <li><a href="thongke.html">Thống Kê</a></li>
+      </ul>
+    </div>
+  
